e87105fffa80a718742dc369ae365b4a6dc33584
Dan Rigby <dan@danrigby.com>
2016-12-10 15:08:47 -0500

    Update usages of several obsolete methods

--- App/Acquaint.XForms/Acquaint.XForms/Views/FloatingActionButtonView.cs
+++ App/Acquaint.XForms/Acquaint.XForms/Views/FloatingActionButtonView.cs
@@ -14,42 +14,42 @@ namespace Acquaint.XForms
     /// </summary>
     public class FloatingActionButtonView : View
     {
-        public static readonly BindableProperty ImageNameProperty = BindableProperty.Create<FloatingActionButtonView,string>( p => p.ImageName, string.Empty);
+        public static readonly BindableProperty ImageNameProperty = BindableProperty.Create(nameof(ImageName), typeof(string), typeof(FloatingActionButtonView), string.Empty);
         public string ImageName 
         {
             get { return (string)GetValue (ImageNameProperty); } 
             set { SetValue (ImageNameProperty, value); } 
         }
 
-        public static readonly BindableProperty ColorNormalProperty = BindableProperty.Create<FloatingActionButtonView,Color>( p => p.ColorNormal, Color.White);
+        public static readonly BindableProperty ColorNormalProperty = BindableProperty.Create(nameof(ColorNormal), typeof(Color), typeof(FloatingActionButtonView), Color.White);
         public Color ColorNormal 
         {
             get { return (Color)GetValue (ColorNormalProperty); } 
             set { SetValue (ColorNormalProperty, value); } 
         }
 
-        public static readonly BindableProperty ColorPressedProperty = BindableProperty.Create<FloatingActionButtonView,Color>( p => p.ColorPressed, Color.White);
+        public static readonly BindableProperty ColorPressedProperty = BindableProperty.Create(nameof(ColorPressed), typeof(Color), typeof(FloatingActionButtonView), Color.White);
         public Color ColorPressed 
         {
             get { return (Color)GetValue (ColorPressedProperty); } 
             set { SetValue (ColorPressedProperty, value); } 
         }
 
-        public static readonly BindableProperty ColorRippleProperty = BindableProperty.Create<FloatingActionButtonView,Color>( p => p.ColorRipple, Color.White);
+        public static readonly BindableProperty ColorRippleProperty = BindableProperty.Create(nameof(ColorRipple), typeof(Color), typeof(FloatingActionButtonView), Color.White);
         public Color ColorRipple 
         {
             get { return (Color)GetValue (ColorRippleProperty); } 
             set { SetValue (ColorRippleProperty, value); } 
         }
 
-        public static readonly BindableProperty SizeProperty = BindableProperty.Create<FloatingActionButtonView,FloatingActionButtonSize>( p => p.Size, FloatingActionButtonSize.Normal);
+        public static readonly BindableProperty SizeProperty = BindableProperty.Create(nameof(Size), typeof(FloatingActionButtonSize), typeof(FloatingActionButtonView), FloatingActionButtonSize.Normal);
         public FloatingActionButtonSize Size 
         {
             get { return (FloatingActionButtonSize)GetValue (SizeProperty); } 
             set { SetValue (SizeProperty, value); } 
         }
 
-        public static readonly BindableProperty HasShadowProperty = BindableProperty.Create<FloatingActionButtonView,bool>( p => p.HasShadow, true);
+        public static readonly BindableProperty HasShadowProperty = BindableProperty.Create(nameof(HasShadow), typeof(bool), typeof(FloatingActionButtonView), true);
         public bool HasShadow 
         {
             get { return (bool)GetValue (HasShadowProperty); } 
